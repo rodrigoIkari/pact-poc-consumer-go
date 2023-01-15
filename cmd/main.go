c9e89509289d4dee90cdf8970935c92d13fa65f4
rodrigoIkari <rodrigo@expersoft.com.br>
2023-01-15 00:04:28 +0000

    Alteração do POC para usar o Fiber

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,15 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-	"os"
-)
+import "github.com/gofiber/fiber/v2"
 
 func main() {
+	app := fiber.New()
 
-	fmt.Println("Iniciando Web Server...")
+	app.Get("/", index)
 
-	http.HandleFunc("/", serveIndex)
-	err := http.ListenAndServe(":8000", nil)
-
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
-
+	app.Listen(":3000")
 }
 
-func serveIndex(w http.ResponseWriter, req *http.Request) {
-	if req.Method != "GET" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Teste"))
+func index(c *fiber.Ctx) error {
+	return c.SendString("Hello World")
 }
