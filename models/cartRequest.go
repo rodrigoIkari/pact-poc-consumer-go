5e1fa7abeb0d9ea0d8cba93ffdaf55ec77feb0c2
Rodrigo Araujo da Silva <rodrigo@expersoft.com.br>
2023-01-25 17:57:57 -0300

    incluindo validação do request

--- models/cartRequest.go
+++ models/cartRequest.go
@@ -1,13 +1,13 @@
 package models
 
 type CartRequest struct {
-	Items            []CartItem `json:"items"`
-	Buyer_Asset_Code string     `json:"buyer-asset-code"`
+	Items            []CartItem `json:"items" validate:""`
+	Buyer_Asset_Code string     `json:"buyer-asset-code"  validate:"required"`
 }
 
 type CartItem struct {
-	SKU               string  `json:"sku"`
-	Quantity          int     `json:"quantity"`
-	Amount            float64 `json:"amount"`
-	Seller_Asset_Code string  `json:"seller-asset-code"`
+	SKU               string  `json:"sku" validade:"required"`
+	Quantity          int     `json:"quantity" validade:"required"`
+	Amount            float64 `json:"amount" validade:"required"`
+	Seller_Asset_Code string  `json:"seller-asset-code" validade:"required"`
 }
